dea97610fe16d2fb04bf654611e0b6a6d3e32d91
Tanaboon Jew <Tanaboon.Jew@gmail.com>
2023-09-11 12:16:31 +0700

    i don't even know what I'm doing.

--- demo/urls.py
+++ demo/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('hello.urls')),
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
+    path('tasks/', include('tasks.urls')),
 ]
